7dff53a8d874c7b0721517a3290c814991b12b9e
Pran Kishore <prankishore@prans-mac-mini.fritz.box>
2018-01-15 19:47:12 +0100

    Initial Commit

---
+++ KisanHubAssignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KisanHubAssignment
+//
+//  Created by Pran Kishore on 15/01/18.
+//  Copyright © 2018 Pran Kishore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
